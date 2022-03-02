b74b35c57a77241c33b6765f7ad35edd818d50fe
ShearmanChua <shearman_chua@hotmail.com>
2022-03-02 11:37:26 +0800

    refractored data code

---
+++ src/common/data_utils.py
@@ -0,0 +1,46 @@
+from tempfile import gettempdir
+import os
+
+import pandas as pd 
+import numpy as np
+
+from datasets import load_dataset, load_from_disk, concatenate_datasets
+
+def merge_clean_unclean(clean:pd.DataFrame(),unclean:pd.DataFrame(),cfg):
+    result = pd.merge(clean,unclean,how="inner",left_on=cfg.rel,right_on=cfg.rel,suffixes=("", "_y"))
+    
+    if cfg.rel is None:
+        result['doc_id'] = result.index
+
+    try:
+        if cfg.rename:
+            result.rename(columns=cfg.rename, inplace=True)
+    except:
+        print("Please check that the rename dictionary passed in matched ")
+
+    result.drop(result.columns.difference([cfg.source,cfg.target,cfg.rel]), 1, inplace=True)
+
+    print(result.info())
+
+    return result
+
+def train_validate_test_split(result,train_split,validation_split):
+    return np.split(result.sample(frac=1, random_state=42), [int(train_split*len(result)), int((1.0-validation_split)*len(result))])
+
+def parquet_and_upload(dataset,dataframe,parquet_name):
+    dataframe.to_parquet(os.path.join(gettempdir(), parquet_name), engine='fastparquet')
+    dataset.add_files(os.path.join(gettempdir(), parquet_name))
+
+def dataset_to_shard(dataset, shard_path="/tmp/dataset", num_shards=8):
+    for index in range(num_shards):
+        dataset.shard(num_shards=num_shards, index=index).save_to_disk(
+            "{}/shard_{}".format(shard_path, index)
+        )
+    return shard_path
+
+
+def shard_to_dataset(shard_path="/tmp/dataset", num_shards=8):
+    dataset = concatenate_datasets(
+        [load_from_disk("{}/shard_{}".format(shard_path, i)) for i in range(num_shards)]
+    )
+    return dataset
--- src/common/huggingface_to_denoising.py
+++ src/common/huggingface_to_denoising.py
@@ -1,125 +1,95 @@
-from clearml import Task, StorageManager, Logger, Dataset
+import os
 
+from clearml import Task, StorageManager, Logger, Dataset
 import pandas as pd 
 import numpy as np
-
-from check_parent_dataset import create_dataset
-from utils import *
-
-PROJECT_NAME = "incubation c4"
-TASK_NAME = "dataset_store_c4_dataset"
-DATASET_PROJECT = "datasets/c4"
-DATASET_NAME = "c4_test_refractored"
-
-task = Task.init(project_name=PROJECT_NAME, task_name=TASK_NAME,output_uri='s3://experiment-logging')
-task.set_base_docker(
-    docker_image="nvidia/cuda:11.4.0-cudnn8-devel-ubuntu20.04",
-)
-
-args = {
-    "clean_dataset_path": "allenai/c4",
-    "clean_dataset_name": "c4",
-    "clean_dataset_variant_name": "en",
-    "clean_dataset_split": "train",
-    "clean_data_files":"en/c4-train.0000[0-1]-of-01024.json.gz",
-    "unclean_dataset_path": "allenai/c4",
-    "unclean_dataset_name": "c4",
-    "unclean_dataset_variant_name": "en.noclean",
-    "unclean_dataset_split": "train",
-    "unclean_data_files":"en.noclean/c4-train.0000[0-1]-of-07168.json.gz"
-}
-
-task.connect(args)
-# task.execute_remotely(queue_name='compute')
-
-print(args)
-
-clean_dataset_path = args['clean_dataset_path']
-clean_dataset_name = args["clean_dataset_name"]
-clean_dataset_variant_name = args["clean_dataset_variant_name"]
-clean_dataset_split = args["clean_dataset_split"]
-clean_data_files = args["clean_data_files"]
-
-unclean_dataset_path = args['unclean_dataset_path']
-unclean_dataset_name = args["unclean_dataset_name"]
-unclean_dataset_variant_name = args["unclean_dataset_variant_name"]
-unclean_dataset_split = args["unclean_dataset_split"]
-unclean_data_files = args["unclean_data_files"]
+import hydra
+from omegaconf import OmegaConf
 
 
-################## dataset_store_c4_datasets #################
-from datasets import load_dataset, load_from_disk, concatenate_datasets
+print(os.path.join("..", "..", "config"))
+@hydra.main(config_path=os.path.join("..", "..", "config"), config_name="ETL_config")
 
-cleaned_dataset = load_dataset(
-    path=clean_dataset_path,name=clean_dataset_variant_name,data_files=clean_data_files,split=clean_dataset_split
-)
+def huggingface_main(cfg):
+    PROJECT_NAME = "incubation c4"
+    TASK_NAME = "dataset_store_c4_dataset"
+    DATASET_PROJECT = "datasets/c4"
+    DATASET_NAME = "c4_test_refractored"
 
-
-uncleaned_dataset = load_dataset(
-    path=unclean_dataset_path,name=unclean_dataset_variant_name,data_files=unclean_data_files,split=unclean_dataset_split
-)
-
-print("Number of samples in {} dataset".format(args['clean_dataset_name']), cleaned_dataset.num_rows)
-print("Number of samples in {} dataset".format(args['unclean_dataset_name']), uncleaned_dataset.num_rows)
-
-
-def dataset_to_shard(dataset, shard_path="/tmp/dataset", num_shards=8):
-    for index in range(num_shards):
-        dataset.shard(num_shards=num_shards, index=index).save_to_disk(
-            "{}/shard_{}".format(shard_path, index)
-        )
-    return shard_path
-
-
-def shard_to_dataset(shard_path="/tmp/dataset", num_shards=8):
-    dataset = concatenate_datasets(
-        [load_from_disk("{}/shard_{}".format(shard_path, i)) for i in range(num_shards)]
+    task = Task.init(project_name=PROJECT_NAME, task_name=TASK_NAME,output_uri='s3://experiment-logging')
+    task.set_base_docker(
+        docker_image="nvidia/cuda:11.4.0-cudnn8-devel-ubuntu20.04",
     )
-    return dataset
 
-## shard dataset and save
-cleaned_shard_path = dataset_to_shard(cleaned_dataset,shard_path="/tmp/cleaned_dataset")
-uncleaned_shard_path = dataset_to_shard(uncleaned_dataset,shard_path="/tmp/uncleaned_dataset")
+    print("Detected config file, initiating task... {}".format(cfg))
 
-## load dataset from shards
-clean_dataset = shard_to_dataset(cleaned_shard_path, num_shards=8)
-unclean_dataset = shard_to_dataset(uncleaned_shard_path, num_shards=8)
+    cfg_dict = OmegaConf.to_container(cfg, resolve=True)
+    task.connect(cfg_dict)
+    task.execute_remotely(queue_name='compute')
 
-clean_df = clean_dataset.to_pandas()
-print(clean_df.info())
-unclean_df = unclean_dataset.to_pandas()
-print(unclean_df.info())
+    from .check_parent_dataset import create_dataset
+    from .data_utils import merge_clean_unclean,dataset_to_shard,shard_to_dataset,train_validate_test_split,parquet_and_upload
 
+    clean_dataset_path = cfg.clean_dataset_path
+    clean_dataset_name = cfg.clean_dataset_name
+    clean_dataset_variant_name = cfg.clean_dataset_variant_name
+    clean_dataset_split = cfg.clean_dataset_split
+    clean_data_files = cfg.clean_data_files
 
-result = merge_clean_unclean(clean_df,unclean_df)
-
-# print(clean_dataset.features)
-# print(clean_dataset.unique('url'))
-
-train, validate, test = train_validate_test_split(result)
-
-dataset = create_dataset(
-    dataset_project=DATASET_PROJECT,
-    dataset_name=DATASET_NAME,
-)
-
-# dataset = Dataset.create(
-#     dataset_project='datasets/c4', dataset_name="c4_raw_clean"
-# )
+    unclean_dataset_path = cfg.unclean_dataset_path
+    unclean_dataset_name = cfg.unclean_dataset_name
+    unclean_dataset_variant_name = cfg.unclean_dataset_variant_name
+    unclean_dataset_split = cfg.unclean_dataset_split
+    unclean_data_files = cfg.unclean_data_files
 
 
-parquet_and_upload(dataset,train,"train.parquet")
-parquet_and_upload(dataset,validate,"validate.parquet")
-parquet_and_upload(dataset,test,"test.parquet")
+    ################## dataset_store_c4_datasets #################
+    from datasets import load_dataset
 
-# train.to_parquet("train.parquet", engine='fastparquet')
-# validate.to_parquet("validate.parquet", engine='fastparquet')
-# test.to_parquet("test.parquet", engine='fastparquet')
+    cleaned_dataset = load_dataset(
+        path=clean_dataset_path,name=clean_dataset_variant_name,data_files=clean_data_files,split=clean_dataset_split
+    )
 
-# dataset.add_files("train.parquet")
-# dataset.add_files("validate.parquet")
-# dataset.add_files("test.parquet")
 
-dataset.upload()
-dataset.finalize()
+    uncleaned_dataset = load_dataset(
+        path=unclean_dataset_path,name=unclean_dataset_variant_name,data_files=unclean_data_files,split=unclean_dataset_split
+    )
+
+    print("Number of samples in {} dataset".format(cfg.clean_dataset_name), cleaned_dataset.num_rows)
+    print("Number of samples in {} dataset".format(cfg.unclean_dataset_name), uncleaned_dataset.num_rows)
+
+    ## shard dataset and save
+    cleaned_shard_path = dataset_to_shard(cleaned_dataset,shard_path="/tmp/cleaned_dataset")
+    uncleaned_shard_path = dataset_to_shard(uncleaned_dataset,shard_path="/tmp/uncleaned_dataset")
+
+    ## load dataset from shards
+    clean_dataset = shard_to_dataset(cleaned_shard_path, num_shards=8)
+    unclean_dataset = shard_to_dataset(uncleaned_shard_path, num_shards=8)
+
+    clean_df = clean_dataset.to_pandas()
+    print(clean_df.info())
+    unclean_df = unclean_dataset.to_pandas()
+    print(unclean_df.info())
+
+
+    result = merge_clean_unclean(clean_df,unclean_df,cfg)
+
+    #split dataset into train, validate, test
+    train, validate, test = train_validate_test_split(result,cfg.train_split,cfg.validation_split)
+
+    dataset = create_dataset(
+        dataset_project=DATASET_PROJECT,
+        dataset_name=DATASET_NAME,
+    )
+
+    parquet_and_upload(dataset,train,"train.parquet")
+    parquet_and_upload(dataset,validate,"validate.parquet")
+    parquet_and_upload(dataset,test,"test.parquet")
+
+
+    dataset.upload()
+    dataset.finalize()
+
+if __name__ == "__main__":
+    huggingface_main()
 
